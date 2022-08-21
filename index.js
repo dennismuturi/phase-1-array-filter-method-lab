63ba2946407e0f14319072f79735626ae8ebb74b
dennismuturi1@gmail.com <dennis@WIN-AVPLU2QPEMC.localdomain>
2022-08-21 16:28:10 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
+const { exportDefaultSpecifier } = require("babel-types");
 // Code your solution here
+
+
+function findMatching(drivers,driverName){
+    let matchedDrivers=[]; 
+    drivers.filter(function (driver){
+        if(driver.toLowerCase() === driverName.toLowerCase()){
+            matchedDrivers.push(driver); 
+        }
+    });
+   return matchedDrivers;
+}
+
+
+function fuzzyMatch(drivers,driverName){
+    let matchedDrivers=[];
+    drivers.filter(driver => {
+        if(driver[0].toLowerCase() === driverName[0].toLowerCase()){
+            matchedDrivers.push(driver);
+        }
+    });
+    return matchedDrivers;
+
+}
+
+
+function matchName(drivers,driverName){
+    let matchedDrivers=[];
+    drivers.filter(driver => {
+        if(driver.name.toLowerCase() === driverName.toLowerCase()){
+            matchedDrivers.push(driver);
+        }
+    });
+    return matchedDrivers;
+
+}
